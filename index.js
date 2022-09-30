a18cbb4844881204ffd65a8608e35d26b3285580
Ahmed-sallam-code <112200911+Ahmed-sallam-code@users.noreply.github.com>
2022-09-30 05:34:24 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,69 @@
+Document
+
+function marioSound(htmlclass){
+document.querySelector("." + htmlclass).addEventListener("click", onClick);
+}
+function onClick(){
+    makeSound();
+    console.log("wruyyyyyyyy");
+}
+
+function makeSound(){
+    var mario= new Audio("audio/marioSE.mp3");
+    mario.play();
+}
+
+marioSound("carousel-control-prev");
+marioSound("carousel-control-next")
+
+////////////////viewport///////////////////
+
+function viewPort(htmlClass,cssClass,i){
+    console.log(i+ htmlClass + cssClass);
+    let prevRatio= 0.0;
+    window.addEventListener("load", (event) => {
+      contentElement = htmlClass;
+      createObserver();
+    }, false);
+    
+    function createObserver() {
+        let observer;
+        let options = {
+          root: null,
+          rootMargin: "0px",
+          threshold: 0
+        };
+      
+        observer = new IntersectionObserver(handleIntersect, options);
+        observer.observe(contentElement);
+      }
+      function handleIntersect(entries, observer) {
+        entries.forEach((entry) => {
+          if (entry.intersectionRatio > prevRatio) {
+            htmlClass.classList.add(cssClass);
+          }
+          prevRatio = entry.intersectionRatio;
+        });
+      }
+
+    }
+    document.querySelectorAll(".assignments").forEach(element => {
+            viewPort(element,"cards-animation");
+    });
+///////////////////////////////////////////////
+
+
+   //Rotating subjects mechanism  
+    document.querySelector(".sun").addEventListener("mouseenter", stopRotationF);
+    function stopRotationF(){
+      document.querySelector(".sun").classList.add("stop");
+      document.querySelectorAll(".planet").forEach(element => {
+        element.classList.add("stop");
+      });
+      setTimeout(function(){
+        document.querySelector(".sun").classList.remove("stop");
+        document.querySelectorAll(".planet").forEach(element => {
+          element.classList.remove("stop");
+        });
+      }, 1000);
+    } /////////////////////////////////
